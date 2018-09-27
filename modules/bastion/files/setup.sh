9c5a621c7ae9cc3af025519a230fb77f350b5bcb
nalbam <me@nalbam.com>
2018-09-27 11:31:51 +0900

    toast.sh/tools

--- modules/bastion/files/setup.sh
+++ modules/bastion/files/setup.sh
@@ -4,5 +4,5 @@
 set -x
 exec > /var/log/user-data.log 2>&1
 
-runuser -l ec2-user -c "curl -sL toast.sh/helper/bastion.sh | bash"
+runuser -l ec2-user -c "curl -sL toast.sh/tools | bash"
 runuser -l ec2-user -c "ssh-keygen -q -f ~/.ssh/id_rsa -N ''"
