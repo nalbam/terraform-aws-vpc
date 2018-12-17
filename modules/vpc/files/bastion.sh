03e5b81ee6a82013b04473eeda3b24686949a691
nalbam <me@nalbam.com>
2018-12-17 13:11:41 +0900

    tools.sh first

--- modules/vpc/files/bastion.sh
+++ modules/vpc/files/bastion.sh
@@ -5,5 +5,5 @@ set -x
 exec > /var/log/user-data.log 2>&1
 
 runuser -l ec2-user -c "ssh-keygen -q -f ~/.ssh/id_rsa -N ''"
+runuser -l ec2-user -c "curl -sL https://raw.githubusercontent.com/opsnow/kops-cui/master/tools.sh | bash"
 runuser -l ec2-user -c "git clone https://github.com/opsnow/kops-cui"
-runuser -l ec2-user -c "bash /home/ec2-user/kops-cui/tools.sh"
