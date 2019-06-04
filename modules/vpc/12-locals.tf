7a18876a0b9bfd174230c80d480b0e6ae7f3216c
Jamje Kim <jamje.kim@bespinglobal.com>
2019-06-04 15:52:53 +0900

    cannot be used with list values

--- modules/vpc/12-locals.tf
+++ modules/vpc/12-locals.tf
@@ -15,14 +15,15 @@ locals {
 
   public_count = "${var.public_subnet_enable ? var.public_subnet_count > 0 ? var.public_subnet_count : length(local.az_names) > 3 ? 3 : length(local.az_names) : 0}"
 
-  public_names = "${length(var.public_subnet_zones) > 0 ? var.public_subnet_zones : data.aws_availability_zones.azs.names}"
+#  public_names = "${length(var.public_subnet_zones) > 0 ? var.public_subnet_zones : data.aws_availability_zones.azs.names}"
+  public_names = "${var.public_subnet_zones}"
 
   public_length = "${length(var.public_subnet_zones) > 0 ? length(var.public_subnet_zones[0]) : length(data.aws_availability_zones.azs.names[0])}"
 
   private_count = "${var.private_subnet_enable ? var.private_subnet_count > 0 ? var.private_subnet_count : length(local.az_names) > 3 ? 3 : length(local.az_names) : 0}"
 
 #  private_names = "${length(var.private_subnet_zones) > 0 ? var.private_subnet_zones : data.aws_availability_zones.azs.names}"
-  private_names = "${length(var.private_subnet_zones) > 0 ? ${var.private_subnet_zones} : ${data.aws_availability_zones.azs.names}}"
+  private_names = "${var.private_subnet_zones}"
 
   private_length = "${length(var.private_subnet_zones) > 0 ? length(var.private_subnet_zones[0]) : length(data.aws_availability_zones.azs.names[0])}"
 }
