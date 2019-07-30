e9a40e31e9c10f834ee582c53dcb53a8e1d9a9c1
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-07-30 10:38:25 +0900

    필요없는 라인 삭제

--- modules/vpc/12-locals.tf
+++ modules/vpc/12-locals.tf
@@ -5,10 +5,6 @@ locals {
 
   full_name = "${var.city}-${var.stage}-${var.name}"
 
-  upper_name = upper(local.full_name)
-
-  lower_name = lower(local.full_name)
-
   az_names = data.aws_availability_zones.azs.names
 
   az_length = length(local.az_names[0])
