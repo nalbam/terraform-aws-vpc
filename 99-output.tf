5caaf3f46f1335d7476e362325812c96b97212d7
nalbam <me@nalbam.com>
2020-01-09 17:01:40 +0900

    add azs output

--- 99-output.tf
+++ 99-output.tf
@@ -12,12 +12,20 @@ output "public_subnet_cidr" {
   value = aws_subnet.public.*.cidr_block
 }
 
+output "public_subnet_azs" {
+  value = aws_subnet.public.*.availability_zone
+}
+
 output "private_subnet_ids" {
   value = aws_subnet.private.*.id
 }
 
 output "private_subnet_cidr" {
   value = aws_subnet.private.*.cidr_block
+}
+
+output "private_subnet_azs" {
+  value = aws_subnet.private.*.availability_zone
 }
 
 output "nat_ip" {
